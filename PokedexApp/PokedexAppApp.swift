22043dbada22ef603eba86ed661af4d336cedf5d
Guy Twig <twigy77@gmail.com>
2024-01-29 20:42:57 +0200

    Initial Commit

---
+++ PokedexApp/PokedexAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexAppApp.swift
+//  PokedexApp
+//
+//  Created by Guy Twig on 29/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct PokedexAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
